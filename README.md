9332a81d378d8761e6f25670906a2ffa738bf84c
John Haugeland <stonecypher@gmail.com>
2016-04-11 11:33:03 -0700

    why am i releasing this

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # nrepl
 Tools for the not-repl in Erlang
+
+These are a bad idea.  You shouldn't use these.  These are generally how I cope 
+with libraries that aren't written with REPL use in mind.  These are jack shims
+to get things to compile.  They're probably wrong and they're certainly evil.
+
+Run.  Run now.
+
+
+
+
+
+Polemic :neckbeard:
+-------------------
+
+`nrepl` is MIT licensed, because viral licenses and newspeak language modification are evil.
+Free is ***only*** free when it's free for everyone.
+
+You still shouldn't use it, though.
---
+++ src/nrepl.erl
@@ -0,0 +1,48 @@
+
+-module(nrepl).
+
+
+
+
+
+-export([
+
+    erl_in_dir/1,
+
+    c_all/1,
+    c_all_with/2
+
+]).
+
+
+
+
+
+erl_in_dir(Dir) ->
+
+    { ok, L } = file:list_dir(Dir),
+
+    F = fun(Li) -> 
+        case lists:reverse(Li) of 
+            "lre." ++ _ -> true; 
+            _           -> false
+        end 
+    end,
+
+    lists:filter(F, L).
+
+
+
+
+
+c_all(Dir) ->
+
+    c_all_with(Dir, []).
+
+
+
+
+
+c_all_with(Dir, Includes) ->
+    
+    [ compile:file(Dir ++ File, [ {i, IDir} || IDir <- Includes ]) || File <- erl_in_dir(Dir) ].
